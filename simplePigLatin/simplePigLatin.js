4ed6ad0ca51651c718691132a95cd4db8ac3bf98
Nav <navkiran.ghuman@gmail.com>
2017-05-01 19:05:34 -0400

    solved

---
+++ simplePigLatin/simplePigLatin.js
@@ -0,0 +1,9 @@
+// Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
+//
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+
+function pigIt(str){
+  return str.split(' ').map(function(el){
+    return el.slice(1) + el.slice(0,1) + 'ay';
+  }).join(' ');
+}
